06121700cb9235168d4e418cabdcd30b9cabdd0a
RunLearn <wg7363@naver.com>
2021-10-13 04:58:31 +0900

    최종

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 import random
 from data import questions
-from function import calculate_probabilites
+from function import calculate_probabilites, question_choice, food_img, best_proba
 
 app = Flask(__name__)
 
@@ -9,9 +9,21 @@ app = Flask(__name__)
 questions_so_far = []
 answers_so_far = []
 
-@app.route('/')
+
+@app.route('/', methods=['GET','POST'])
 def index():
     global questions_so_far, answers_so_far
+
+    # if request.method == 'POST':
+    #     value = request.form['reset']
+    #     value = str(value)
+    #     print(value)
+    #
+    # if value:
+    #     print('게임을 다시 시작합니다.')
+
+
+
 
     question = request.args.get('question')
     answer = request.args.get('answer')
@@ -26,11 +38,15 @@ def index():
 
     result = sorted(probabilities, key=lambda p: p['probability'], reverse=True)[0]
 
-    if len(questions_left) == 0 or (result['probability'] > 0.9):
-
-        return render_template('index.html', result=result['name'], probability=result['probability'])
+    # if len(questions_left) == 0 or (result['probability'] > 0.7):
+    if len(questions_left) == 0 or best_proba(probabilities) == True:
+        return render_template('index.html', result=result['name'], probability=result['probability'],result_img = food_img(result['name']))
     else:
-        next_question = random.choice(questions_left)
+        if len(questions_left) % 2 == 1:
+            next_question = random.choice(questions_left)
+        else:
+            next_question = question_choice(questions_left)
+
     return render_template('index.html', question=next_question, question_text=questions[next_question],probability=probabilities)
 
 
--- function.py
+++ function.py
@@ -1,4 +1,5 @@
-from data import foods
+
+from data import foods, food_df
 import numpy as np
 
 
@@ -50,6 +51,54 @@ def food_answer(food, question):
         return food['answers'][question]
     return 0.5
 
-def question_choice(question,answer):
-    return 0
+#엔트로피를 이용하여 최적의 질문 선별
 
+# 엔트로피
+def entropy(target_col):
+    elements, counts = np.unique(target_col, return_counts = True)
+    entropy = -np.sum([(counts[i]/np.sum(counts))*np.log2(counts[i]/np.sum(counts)) for i in range(len(elements))])
+    return entropy
+
+# 정보이득
+def InfoGain(data, split_attribute_name, target_name):
+    # 전체 엔트로피 계산
+    total_entropy = entropy(data[target_name])
+    # print('Entropy(D) = ', round(total_entropy, 5))
+
+    # 가중 엔트로피 계산
+    vals, counts = np.unique(data[split_attribute_name], return_counts=True)
+    Weighted_Entropy = np.sum([(counts[i] / np.sum(counts)) *
+                               entropy(data.where(data[split_attribute_name] == vals[i]).dropna()[target_name])
+                               for i in range(len(vals))])
+    # print('H(', split_attribute_name, ') = ', round(Weighted_Entropy, 5))
+
+    # 정보이득 계산
+    Information_Gain = total_entropy - Weighted_Entropy
+    return Information_Gain
+
+def question_choice(questions):
+    best_Info = 0
+    for i in questions:
+        check = InfoGain(food_df,i,'name')
+        if check > best_Info:
+            best_Info = check
+            best_question = i
+    else:
+        print(best_question,best_Info)
+        toggle_random = 1
+        return best_question
+
+def food_img(result):
+    return (f'img/{result}.png')
+
+def best_proba(probabilities):
+    probabilities_list=[]
+    for i in probabilities:
+        probabilities_list.append(i['probability'])
+
+    max_proba=max(probabilities_list)
+    avg_proba=sum(probabilities_list)/len(probabilities_list)
+    if max_proba >= avg_proba+0.5:
+        return True
+    else:
+        return False
